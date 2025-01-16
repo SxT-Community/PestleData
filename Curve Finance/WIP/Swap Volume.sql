4107bf9ad045ce90a0fcdbbb76e95902869d41a0
chilaraisxt <chilarai.mushahary@spaceandtime.io>
2025-01-16 07:25:24 +0530

    Swap volume

---
+++ Curve Finance/WIP/Swap Volume.sql
@@ -0,0 +1,10 @@
+SELECT 
+    DATE(block_timestamp) AS date,
+    SUM(amount_in_usd) AS total_amount_in_usd
+FROM (
+    SELECT * 
+    FROM ethereum.defi.ez_dex_swaps
+    WHERE platform = 'curve'
+) AS curve_swaps
+GROUP BY date
+ORDER BY date ASC;
